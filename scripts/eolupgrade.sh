19eb31aea9596bcfe17c65aabf19bb4c38656b81
Jesse Horner <jrhorner@pm.me>
2022-07-24 16:35:36 -0400

    Added a few CYA & QOL improvements. Included noninteractive upgrade to 21.10.

--- scripts/eolupgrade.sh
+++ scripts/eolupgrade.sh
@@ -6,20 +6,33 @@ if [ $(whoami) != "root" ]; then
     exit 1
 fi
 
+# Create a backup of /etc/apt/sources.list
+cp /etc/apt/sources.list /etc/apt/sources.list.backup
 # Change mirror source to old-releases.ubuntu.com since we're running an EOL version
 sed -E -i 's|http://ports.ubuntu.com/ubuntu-ports/?|http://old-releases.ubuntu.com/ubuntu/|g' /etc/apt/sources.list
 
 # Update package lists from the new source and install any available updates
-apt-get update
-apt-get upgrade -y
-apt-get dist-upgrade -y
+export DEBIAN_FRONTEND=noninteractive
+apt update
+apt upgrade -yq
+apt dist-upgrade -yq
 # Autoremove any packages no longer required
-apt autoremove -y
+apt autoremove -yq
+
+# Set default behavoir for release upgrader to "lts"
+sed -E -i 's/Prompt=(never|normal|lts)/Prompt=normal/g' /etc/update-manager/release-upgrades
 
 # Bypass unsupported upgrade check in `do-release-upgrade` script
 sed -i 's/continue/pass/g' /usr/lib/python3/dist-packages/UpdateManager/Core/MetaRelease.py # line 315
 
+# Perform the upgrade to 21.10 without interactive prompts, autoremove any packages no longer required
+do-release-upgrade -f DistUpgradeViewNonInteractive
+apt autoremove -yq
+
+# Print release information to terminal to verify
+cat /etc/*release
+
 printf "\n%s\n\t%s\n\t%s\n\n" \
-"To perform the upgrade, create a tmux session and run the interactive upgrade script:" \
+"To perform the upgrade to 22.04 LTS, reboot then run the upgrade script in a tmux session:" \
     "tmux new -s release-upgrade" \
-    "sudo do-release-upgrade"
+    "sudo do-release-upgrade -f DistUpgradeViewNonInteractive"
