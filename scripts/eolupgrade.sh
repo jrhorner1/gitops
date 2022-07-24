9435eb40b61ce5bc8203a58af98a98f360fd359e
Jesse Horner <jrhorner@pm.me>
2022-07-24 10:58:18 -0400

    Adding a scripts folder and this EOL upgrade script because I can't pay attention to which release of Ubuntu I'm running and when it is no longer supported.

---
+++ scripts/eolupgrade.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Sanity check for root
+if [ $(whoami) != "root" ]; then
+    echo 'Try: sudo !!'
+    exit 1
+fi
+
+# Change mirror source to old-releases.ubuntu.com since we're running an EOL version
+sed -i 's|http://ports.ubuntu.com/ubuntu-ports/|http://old-releases.ubuntu.com/ubuntu/|g' /etc/apt/sources.list
+
+# Update package lists from the new source and install any available updates
+apt-get update
+apt-get upgrade -y
+apt-get dist-upgrade -y
+
+# Bypass unsupported upgrade check in `do-release-upgrade` script
+sed -i 's/continue/pass/g' /usr/lib/python3/dist-packages/UpdateManager/Core/MetaRelease.py # line 315
+
+printf "%s\n\t%s\n\t%s" "To perform the upgrade, create a tmux session and run the interactive upgrade script:" "tmux new -s release-upgrade" "sudo do-release-upgrade"
